ac067dcaac78d8575ff18a7fbcff4e0f6961113c
erifranck <erifranckjose@gmail.com>
2017-06-15 02:20:57 -0400

    crea manejo de rutas

---
+++ src/routes.js
@@ -0,0 +1,32 @@
+// We only need to import the modules necessary for initial render
+import HelloWorld from "./Pages/HelloWorld"
+import Layout from "./Pages/Layout"
+
+/*  Note: Instead of using JSX, we recommend using react-router
+    PlainRoute objects to build route definitions.   */
+
+export const createRoutes = (store) => ({
+  path        : '/',
+  component   : Layout,
+  indexRoute  : HelloWorld,
+})
+
+/*  Note: childRoutes can be chunked or otherwise loaded programmatically
+    using getChildRoutes with the following signature:
+
+    getChildRoutes (location, cb) {
+      require.ensure([], (require) => {
+        cb(null, [
+          // Remove imports!
+          require('./Counter').default(store)
+        ])
+      })
+    }
+
+    However, this is not necessary for code-splitting! It simply provides
+    an API for async route definitions. Your code splitting should occur
+    inside the route `getComponent` function, since it is only invoked
+    when the route exists and matches.
+*/
+
+export default createRoutes

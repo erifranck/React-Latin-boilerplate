0e490af2e92c457567e619ccecb6c8ea66b576a8
erifranck <erifranjose@hotmail.com>
2017-06-17 17:53:48 -0400

    configure webpack dashboard

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,10 +1,13 @@
 var path = require("path");
-var DashboardPlugin = require('webpack-dashboard/plugin');
 var webpack = require('webpack');
 
 module.exports = {
   devtool: "eval",
-  entry: path.resolve(__dirname, '../src/client.js'),
+  entry: [
+    'webpack/hot/dev-server',
+    'webpack-hot-middleware/client',
+    path.resolve(__dirname, '../src/client.js')
+  ],
   output: {
     path: path.resolve(__dirname, '../public/assets'),
     publicPath: path.resolve(__dirname, '../src'),
@@ -29,18 +32,19 @@ module.exports = {
           loader: "style-loader" // creates style nodes from JS strings
         }, {
           loader: "css-loader", // translates CSS into CommonJS
-          options: {sourceMap: true}
+          options: {
+            sourceMap: true
+          }
         }, {
           loader: "sass-loader", // compiles Sass to CSS
-          options: {sourceMap: true}
+          options: {
+            sourceMap: true
+          }
         }]
       },
     ]
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
-    new DashboardPlugin({
-      port: 3001
-    })
   ]
 };
--- server.js
+++ server.js
@@ -1,27 +1,40 @@
 var config = require("./config/webpack.dev.js");
+var express = require("express");
 var webpack = require("webpack");
-var webpackDevServer = require("webpack-dev-server");
+var webpackDevMiddleware = require("webpack-dev-middleware");
+var webpackHotMiddleware = require("webpack-hot-middleware");
+var webpackDashboard = require("webpack-dashboard");
+var webpackDashboardPlugin = require("webpack-dashboard/plugin");
+var Dashboard = new webpackDashboard();
 var compiler = webpack(config);
 var path = require("path");
-var server = new webpackDevServer(compiler, {
+var app = express();
+compiler.apply(new webpackDashboardPlugin( Dashboard.setData ));
+app.use(express.static('public'));
+app.use(webpackDevMiddleware(compiler, {
   hot: true,
   contentBase: path.join(__dirname, "public"),
   publicPath: "/assets",
   filename: "bundle.js",
   stats: {
-      assets: true,
-      children: false,
-      chunks: false,
-      hash: false,
-      modules: true,
-      publicPath: false,
-      timings: true,
-      version: false,
-      warnings: true,
-      colors: {
-        green: '\u001b[32m',
-      },
+    assets: true,
+    children: false,
+    chunks: false,
+    hash: false,
+    modules: true,
+    publicPath: false,
+    timings: true,
+    version: false,
+    warnings: true,
+    colors: {
+      green: '\u001b[32m',
     },
-  // compress: true,
+  },
+}));
+app.use(webpackHotMiddleware(compiler, {
+  log: console.log
+}));
+app.use((req, res) => {
+  req.send("index.html")
 });
-server.listen(9000);
+app.listen(9000, console.log("listening on 3000"));
